56c979663ea7276ff512f11bf81772d98c1d3975
yusuke.aoki <yusuke.aoki@optim.co.jp>
2021-02-24 10:55:07 +0900

    コメント追加

--- myapp/app.js
+++ myapp/app.js
@@ -24,36 +24,52 @@ app.use(express.static(path.join(__dirname, 'public')))
 // ここから追記
 // ===================================
 
+/**
+ * 認証情報
+ */
 const issuer = 'https://auth.optim.cloud'
 const baseURL = 'http://localhost:3000'
 const clientId = typeof process.env.CLIENT_ID === "undefined" ? '' : process.env.CLIENT_ID
 const clientSecret = typeof process.env.CLIENT_SECRET === "undefined" ? '' : process.env.CLIENT_SECRET
+
+/**
+ * 初期化
+ */
 app.use(auth({
   issuerBaseURL: issuer,
-  baseURL: baseURL,
+  baseURL: baseURL,         // このアプリケーションのデプロイ先URL（デバッグ時はlocalhostを利用）
   clientID: clientId,
   secret: clientSecret,
   idpLogout: true,
-  authRequired: true,
+  authRequired: true,       // すべてのRouteにAuthMiddlewareを導入
   routes:{
-    callback: "/oauth2/callback"
+    callback: "/oauth2/callback"    // Callback Path をカスタム
   },
-  authorizationParams: {
+  authorizationParams: {            // OAuth2.0 フローを行う
     response_type: 'code',
-    scope: 'openid user.profile',
+    scope: 'openid user.profile',   // OAuth2.0 で認可させるスコープ
   }
 }))
 
-
+/**
+ * Example API （アカウント情報取得）
+ */
 const accountAPI = 'https://accounts.optimcloudapis.com'
 app.get('/', async (req, res) => {
+
+  // DBにUser情報を持つ場合は
+  // req.oidc.sub と User情報の紐付けを行うことが出来る
+  
+  // 認可レスポンスに含まれたAccessTokenを利用する
   let { token_type, access_token } = req.oidc.accessToken;
-  console.log(access_token)
+
+  // AccountAPIを叩く
   const me = await fetch(accountAPI + '/v2/me', {
     headers: {
       Authorization: `${token_type} ${access_token}`,
     },
   });
+  
   res.json(await me.json());
 });
 
